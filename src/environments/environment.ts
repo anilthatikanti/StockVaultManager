d461f478f61ec77b906e5f66f711badaf42177c9
anilthatikanti <107552554+anilthatikanti@users.noreply.github.com>
2025-05-26 15:00:37 +0530

    Update environment.ts
    updated envirnoment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,1 @@
-export const environment = {
-    firebaseConfig: {
-      apiKey: 'AIzaSyB7O_hkpDS266BbZRQtWFzOrn0jK2y3K6U',
-      authDomain:'authentication-bf2a7.firebaseapp.com',
-      projectId: 'authentication-bf2a7',
-      storageBucket: 'authentication-bf2a7.appspot.com',
-      messagingSenderId: 'undefined',
-      appId: '1:375284713381:web:083f88f5ba2a2fab658a4c',
-      measurementId: 'G-W89BM4HVZ1'
-    },
-    production: true,
-    useEmulators: false,
-  }
 
-  export const WEB_APP_URL = 'http://localhost:4200';
-export const SERVER_URL = 'http://localhost:3000';
-export const WEB_SOCKET = 'ws://localhost:3000';
